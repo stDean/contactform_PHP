01fab2126f546cabb485a0008573913eb88a73a7
Toba-dean <tobaogundimu@gmail.com>
2023-07-10 17:23:03 +0100

    added the javascrit logic gor toggling between register and login UI

--- public/script.js
+++ public/script.js
@@ -1,1 +1,39 @@
-console.log("Hello America")
+const container = document.querySelector(".container");
+const showHidePwd = Array.from(document.querySelectorAll(".showHide"));
+const allPasswordFields = Array.from(document.querySelectorAll(".password"));
+const signUpText = document.querySelector(".signup-link");
+const loginText = document.querySelector(".login-link");
+const form = document.querySelector(".signUp");
+const userInput = document.querySelector("#user-input");
+const emailInput = document.querySelector("#email-input");
+const passInput = document.querySelector("#pass-input");
+const confirmInput = document.querySelector("#confirm-input");
+
+// toggling password icon
+showHidePwd.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    allPasswordFields.forEach((field) => {
+      if (field.type === "password") {
+        field.type = "text";
+
+        showHidePwd.forEach((item) => {
+          item.classList.replace("fa-eye-slash", "fa-eye");
+        });
+      } else {
+        field.type = "password";
+        showHidePwd.forEach((item) => {
+          item.classList.replace("fa-eye", "fa-eye-slash");
+        });
+      }
+    });
+  });
+});
+
+// adding active class to change the form from a login one to a register one and vice versa
+signUpText.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginText.addEventListener("click", () => {
+  container.classList.remove("active");
+});
